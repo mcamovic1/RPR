5df1ef0b1f2029ac844e49b7cb02c5f96cde8451
mcamovic1 <115951662+mcamovic1@users.noreply.github.com>
2023-11-05 21:30:58 +0100

    Update ImenikTest.java for method izGrada()

--- test/java/org/example/ImenikTest.java
+++ test/java/org/example/ImenikTest.java
@@ -28,5 +28,24 @@ class ImenikTest {
 
     @org.junit.jupiter.api.Test
     void izGrada() {
+        Imenik imenik = new Imenik();
+
+        FiksniBroj fiksniBroj1 = new FiksniBroj(Grad.SARAJEVO, "123-456");
+        FiksniBroj fiksniBroj2 = new FiksniBroj(Grad.SARAJEVO, "789-012");
+        MobilniBroj mobilniBroj = new MobilniBroj(61, "345-678");
+
+        imenik.dodaj("Elma", fiksniBroj1);
+        imenik.dodaj("Erna", fiksniBroj2);
+        imenik.dodaj("Melida", mobilniBroj);
+
+        Set<String> rezultat = null;
+        try {
+            rezultat = imenik.izGrada(Grad.SARAJEVO);
+        } catch (NijePronadjenNijedan e) {
+            throw new RuntimeException(e);
+        }
+        assertTrue(rezultat.contains("Elma"));
+        assertTrue(rezultat.contains("Erna"));
+        assertFalse(rezultat.contains("Melida"));
     }
 }
